c3b71be34c353ba9c5483c277148faed90233ec9
DrHurel <jeremyhurel34@gmail.com>
2023-12-28 10:02:45 +0100

    reduce reading complexity

--- dico/dices.go
+++ dico/dices.go
@@ -7,8 +7,18 @@ import (
 )
 
 type Dices [16]Dice
-type LetterOptionMap map[rune][]rune
+type LetterOptionMap map[string][]string
 type RemovePatern map[string][][]string
+
+func checkFace(l rune, lomap LetterOptionMap, dice Dice) {
+	check := make(map[rune]bool)
+	for _, face := range dice {
+		if !check[face] && face != l {
+			lomap[string(l)] = append(lomap[string(l)], string(face))
+			check[face] = true
+		}
+	}
+}
 
 func (this Dices) LetterOption(path ...string) LetterOptionMap {
 	allLetter := "ABCDEFGHIJKLMNOPQRSTUVWYXZ"
@@ -19,14 +29,8 @@ func (this Dices) LetterOption(path ...string) LetterOptionMap {
 	for _, l := range allLetter {
 
 		for _, dice := range this {
-			check := make(map[rune]bool)
 			if dice.Contains(l) {
-				for _, face := range dice {
-					if !check[face] && face != l {
-						res[l] = append(res[l], face)
-						check[face] = true
-					}
-				}
+				checkFace(l, res, dice)
 			}
 		}
 	}
@@ -44,10 +48,10 @@ func (this Dices) LetterOption(path ...string) LetterOptionMap {
 	return res
 }
 
-func numberOfIteration(LetterOption []rune, target rune) int {
+func numberOfIteration(LetterOption []string, target rune) int {
 	count := 0
 	for _, l := range LetterOption {
-		if l == target {
+		if l == string(target) {
 			count++
 		}
 	}
@@ -66,11 +70,11 @@ func (this Dices) RemoveIfPick(out ...string) RemovePatern {
 	LetterOptionMap := this.LetterOption(filepath.Join(tmp...))
 	res := make(RemovePatern, 0)
 	for _, letter := range allLetter {
-		n := letterMaxNumber[letter]
+		n := letterMaxNumber[string(letter)]
 		res[string(letter)] = make([][]string, n)
 		for _, letter2 := range allLetter {
 			if letter2 != letter {
-				if count := numberOfIteration(LetterOptionMap[letter], letter2); count != 0 {
+				if count := numberOfIteration(LetterOptionMap[string(letter)], letter2); count != 0 {
 					res[string(letter)][n-count] = append(res[string(letter)][n-count], string(letter2))
 				}
 			}
@@ -91,15 +95,15 @@ func (this Dices) RemoveIfPick(out ...string) RemovePatern {
 
 }
 
-func (this Dices) LetterMaxNumber(path ...string) map[rune]int {
+func (this Dices) LetterMaxNumber(path ...string) map[string]int {
 
-	res := make(map[rune]int)
+	res := make(map[string]int)
 	for _, dice := range this {
 		check := make(map[rune]bool)
 		for _, face := range dice {
 			if !check[face] {
 				check[face] = true
-				res[face]++
+				res[string(face)]++
 			}
 		}
 	}
